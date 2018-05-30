5646b1da817396d4306e22f2484387303eb7c06d
Brandon Alejandro Mosqueda González <brandonMG_1698@hotmail.com>
2018-05-29 23:32:24 -0500

    Primer commit, todo funcionando, genera bien el árbol binario de la expresión y sus notaciones inorder, postorder, preorder, además de que resulve la expresión en preorder

---
+++ Notaciones polakas/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Notaciones_polakas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtExpresion = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblPreOrder = new System.Windows.Forms.Label();
+            this.lblPostOrder = new System.Windows.Forms.Label();
+            this.lblInOrder = new System.Windows.Forms.Label();
+            this.btnEvaluatePreOrder = new System.Windows.Forms.Button();
+            this.txtStackNumbers = new System.Windows.Forms.RichTextBox();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtExpresion
+            // 
+            this.txtExpresion.Location = new System.Drawing.Point(26, 12);
+            this.txtExpresion.Name = "txtExpresion";
+            this.txtExpresion.Size = new System.Drawing.Size(353, 26);
+            this.txtExpresion.TabIndex = 0;
+            this.txtExpresion.Text = "6-3*4*3*2/8-6/2";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.AutoSize = true;
+            this.btnGenerate.Location = new System.Drawing.Point(26, 54);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(78, 30);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generar";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblPreOrder
+            // 
+            this.lblPreOrder.AutoSize = true;
+            this.lblPreOrder.Location = new System.Drawing.Point(22, 107);
+            this.lblPreOrder.Name = "lblPreOrder";
+            this.lblPreOrder.Size = new System.Drawing.Size(0, 20);
+            this.lblPreOrder.TabIndex = 2;
+            // 
+            // lblPostOrder
+            // 
+            this.lblPostOrder.AutoSize = true;
+            this.lblPostOrder.Location = new System.Drawing.Point(22, 147);
+            this.lblPostOrder.Name = "lblPostOrder";
+            this.lblPostOrder.Size = new System.Drawing.Size(0, 20);
+            this.lblPostOrder.TabIndex = 3;
+            // 
+            // lblInOrder
+            // 
+            this.lblInOrder.AutoSize = true;
+            this.lblInOrder.Location = new System.Drawing.Point(22, 204);
+            this.lblInOrder.Name = "lblInOrder";
+            this.lblInOrder.Size = new System.Drawing.Size(0, 20);
+            this.lblInOrder.TabIndex = 4;
+            // 
+            // btnEvaluatePreOrder
+            // 
+            this.btnEvaluatePreOrder.AutoSize = true;
+            this.btnEvaluatePreOrder.Location = new System.Drawing.Point(457, 245);
+            this.btnEvaluatePreOrder.Name = "btnEvaluatePreOrder";
+            this.btnEvaluatePreOrder.Size = new System.Drawing.Size(83, 34);
+            this.btnEvaluatePreOrder.TabIndex = 5;
+            this.btnEvaluatePreOrder.Text = "PreOrder";
+            this.btnEvaluatePreOrder.UseVisualStyleBackColor = true;
+            this.btnEvaluatePreOrder.Click += new System.EventHandler(this.btnEvaluatePreOrder_Click);
+            // 
+            // txtStackNumbers
+            // 
+            this.txtStackNumbers.Location = new System.Drawing.Point(459, 12);
+            this.txtStackNumbers.Name = "txtStackNumbers";
+            this.txtStackNumbers.Size = new System.Drawing.Size(81, 227);
+            this.txtStackNumbers.TabIndex = 6;
+            this.txtStackNumbers.Text = "";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Location = new System.Drawing.Point(401, 15);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(0, 20);
+            this.lblCurrent.TabIndex = 8;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 568);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.txtStackNumbers);
+            this.Controls.Add(this.btnEvaluatePreOrder);
+            this.Controls.Add(this.lblInOrder);
+            this.Controls.Add(this.lblPostOrder);
+            this.Controls.Add(this.lblPreOrder);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtExpresion);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtExpresion;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblPreOrder;
+        private System.Windows.Forms.Label lblPostOrder;
+        private System.Windows.Forms.Label lblInOrder;
+        private System.Windows.Forms.Button btnEvaluatePreOrder;
+        private System.Windows.Forms.RichTextBox txtStackNumbers;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Notaciones polakas/Form1.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Notaciones_polakas
+{
+    public partial class Form1 : Form
+    {
+        //model to PolishNotation (tree) class
+        public class Node
+        {
+            public char Val { get; set; }
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+
+            public Node(char val)
+            {
+                this.Val = val;
+            }
+
+            public bool hasLeft() { return LeftChild != null; }
+            public bool hasRight() { return RightChild != null; }
+        }
+
+        //model to stack class
+        public class Element
+        {
+            public Node Val { get; set; }
+            public Element Next { get; set; }
+
+            public Element(Node val)
+            {
+                this.Val = val;
+            }
+        }
+
+        public class Num
+        {
+            public double Val { get; set; }
+            public Num Next { get; set; }
+
+            public Num(double val)
+            {
+                this.Val = val;
+            }
+        }
+
+        public class StackNumbers
+        {
+            public Num first;
+
+            public void push(Num newValue)
+            {
+                if (isEmpty())
+                    first = newValue;
+                else
+                {
+                    newValue.Next = first;
+                    first = newValue;
+                }
+            }
+
+            public Num pop()
+            {
+                if (!isEmpty())
+                {
+                    Num temp = first;
+                    first = first.Next;
+                    return temp;
+                }
+
+                return null;
+            }
+
+            public Num peek() { return first; }
+
+            public bool isEmpty() { return first == null; }
+
+            public string list()
+            {
+                string stack = "";
+                Num temp = first;
+                while(temp != null)
+                {
+                    stack = temp.Val + "\n" + stack;
+                    temp = temp.Next;
+                }
+                return stack;
+            }
+        }
+
+        public class Queue
+        {
+            private Element first;
+            private Element last;
+
+            public Element peek() { return first; }
+
+            public void add(Element newElement)
+            {
+                if (first != null)
+                {
+                    last.Next = newElement;
+                    last = newElement;
+                }
+                else
+                {
+                    first = newElement;
+                    last = newElement;
+                }
+            }
+
+            public Element poll()
+            {
+                if (first != null)
+                {
+                    if (first.Next != null)
+                    {
+                        Element temp = first;
+                        first = first.Next;
+                        return temp;
+                    }
+                    else
+                    {
+                        Element temp = first;
+                        first = null;
+                        last = null;
+                        return temp;
+                    }
+                }
+                return null;
+            }
+
+            public Element pollLast()
+            {
+                if (first != null)
+                {
+                    if (first.Next != null)
+                    {
+                        Element temp = first;
+                        while (temp.Next.Next != null)
+                            temp = temp.Next;
+                        Element result = last;
+                        last = temp;
+                        temp.Next = null;
+                        return result;
+                    }
+                    else
+                    {
+                        Element temp = first;
+                        first = null;
+                        last = null;
+                        return temp;
+                    }
+                }
+                return null;
+            }
+
+            public string list()
+            {
+                if (first != null)
+                {
+                    string processString = first.ToString();
+                    Element temp = first;
+                    while (temp.Next != null)
+                    {
+                        processString += temp.ToString();
+                        temp = temp.Next;
+                    }
+                    return processString;
+                }
+                else
+                    return "No se ha agregado ningún proceso a la cola";
+            }
+        }
+
+        public class PolishNotation
+        {
+            private Queue queue;
+            public char[] Expresion;
+
+            public StackNumbers stack { get; }
+            private Node root;
+            private char[] operands = { '/', '*', '+', '-' };
+
+            public PolishNotation(string expresion)
+            {
+                this.queue = new Queue();
+                this.Expresion = new char[expresion.Length];
+
+                stack = new StackNumbers();
+
+                expresion = expresion.Trim();
+                for (int i = 0; i < expresion.Length; i++)
+                    this.Expresion[i] = expresion[i];
+            }
+
+            public void generateTree()
+            {
+                for (int i = 0; i < operands.Length; i += 2)
+                {
+                    for (int j = 0; j < Expresion.Length; j++)
+                    {
+                        if (Expresion[j] == operands[i] || Expresion[j] == operands[i + 1])
+                        {
+                            Node root = new Node(Expresion[j]);
+                            Node left;
+                            Node right;
+                            try {
+                                if (Expresion[j - 1] == '|' && Expresion[j + 1] == '|' && (i == 2 || i == 3))
+                                {
+                                    left = queue.pollLast().Val;
+                                    right = queue.poll().Val;
+                                }
+                                else
+                                {
+                                    if (char.IsNumber(Expresion[j - 1]))
+                                    {
+                                        left = new Node(Expresion[j - 1]);
+                                    }
+                                    else if (Expresion[j - 1] == '|')
+                                    {
+                                        left = queue.poll().Val;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Caracter izquierdo incorrecto");
+                                        return;
+                                    }
+
+                                    if (Char.IsNumber(Expresion[j + 1]))
+                                    {
+                                        right = new Node(Expresion[j + 1]);
+                                    }
+                                    else if (Expresion[j + 1] == '|')
+                                    {
+                                        right = queue.poll().Val;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Caracter derecho incorrecto");
+                                        return;
+                                    }
+                                }
+
+                                root.LeftChild = left;
+                                root.RightChild = right;
+                                Expresion[j] = '|';
+                                Expresion[j - 1] = '|';
+                                Expresion[j + 1] = '|';
+                                j++;
+
+                                queue.add(new Element(root));
+                            } catch (Exception err) {
+                                Console.WriteLine("Fuera de los límites del arreglo");
+                                Console.WriteLine(err);
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                root = queue.poll().Val;
+            }
+
+            public string getPreOrder()
+            {
+                string preOrder = "";
+
+                generatePreOrder(root, ref preOrder);
+
+                return preOrder;
+            }
+
+            public string getPostOrder()
+            {
+                string postOrder = "PostOrder:   ";
+
+                generatePostOrder(root, ref postOrder);
+
+                return postOrder;
+            }
+
+            public string getInOrder()
+            {
+                string inOrder = "InOrder:   ";
+
+                generateInOrder(root, ref inOrder);
+
+                return inOrder;
+            }
+
+            public void generatePreOrder(Node currentRoot, ref string str)
+            {
+                str += currentRoot.Val + "";
+                if (currentRoot.hasLeft())
+                    generatePreOrder(currentRoot.LeftChild, ref str);
+
+                if (currentRoot.hasRight())
+                    generatePreOrder(currentRoot.RightChild, ref str);
+            }
+
+            public void generatePostOrder(Node currentRoot, ref string str)
+            {
+                if (currentRoot.hasLeft())
+                    generatePostOrder(currentRoot.LeftChild, ref str);
+
+                if (currentRoot.hasRight())
+                    generatePostOrder(currentRoot.RightChild, ref str);
+
+                str += currentRoot.Val + " ";
+            }
+
+            public void generateInOrder(Node currentRoot, ref string str)
+            {
+                if (currentRoot.hasLeft())
+                    generateInOrder(currentRoot.LeftChild, ref str);
+
+                str += currentRoot.Val + " ";
+
+                if (currentRoot.hasRight())
+                    generateInOrder(currentRoot.RightChild, ref str);
+
+
+            }
+
+            public void evaluatePreOrder(char operandOrNum)
+            {
+                if(char.IsNumber(operandOrNum))
+                {
+                    stack.push(new Num(Convert.ToInt32(operandOrNum + "")));
+                }
+                else
+                {
+                    double x = stack.pop().Val;
+                    double y = stack.pop().Val;
+
+                    stack.push(new Num(getOperationResult(operandOrNum, x, y)));
+                }
+            }
+
+            private double getOperationResult(char operand, double x, double y)
+            {
+                switch (operand)
+                {
+                    case '+':
+                        return x + y;
+                    case '-':
+                        return x - y;
+                    case '/':
+                        return x / y;
+                    case '*':
+                        return x * y;
+                    default:
+                        throw new Exception("Operdor inválido");
+                }
+            }
+        }
+
+        PolishNotation notation;
+        string preOrder;
+        int index;
+        int indexPost;
+        public Form1()
+        {
+            InitializeComponent();
+            preOrder = "";
+        }
+
+        public void validateExpresion()
+        {
+            //Method that validate the users don´t send others characters, only numbers and * / + -, delete
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            notation = new PolishNotation(txtExpresion.Text);
+            notation.generateTree();
+            preOrder = notation.getPreOrder();
+
+            lblPreOrder.Text = "PreOrder:   " + preOrder;
+            lblPostOrder.Text = notation.getPostOrder();
+            lblInOrder.Text = notation.getInOrder();
+        }
+
+        private void btnEvaluatePreOrder_Click(object sender, EventArgs e)
+        {
+            index = preOrder.Length - 1;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            notation.evaluatePreOrder(preOrder[index]);
+            txtStackNumbers.Text = notation.stack.list();
+            lblCurrent.Text = preOrder[index] + "";
+            index--;
+            if (index < 0)
+                timer.Stop();
+        }
+    }
+}
